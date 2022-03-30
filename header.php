69e4fe52f94874be64b46fd9b128f8dd93e1aad7
sprosenblum <81576433+sprosenblum@users.noreply.github.com>
2022-03-30 13:14:47 -0400

    updating navbar

--- header.php
+++ header.php
@@ -10,22 +10,24 @@
 
     <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-link active" aria-current="page" href="#home">Home</a>
-                    <a class="nav-link" href="#">Features</a>
-                    <a class="nav-link" href="#">Pricing</a>
-                    <a class="nav-link" href="#">Disabled</a>
+                    <a class="nav-link" href="#">Authors</a>
+                    <a class="nav-link" href="#">Young-Adult</a>
+                    <a class="nav-link" href="#">Adult</a>
                 </div>
-            <form class="d-flex">
-                <?php get_search_form(); ?>
-            </form>
             </div>
         </div>
+
+        <form class="d-flex ">
+                <?php get_search_form(); ?>
+            </form>
+
     </nav>
 
     <?php wp_head(); ?>
