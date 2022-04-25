c3ad7e3bc81ee68a93aaba55e29bdfd87dcd8ce6
Sarah Rosenblum <rosenblum.30@osu.edu>
2022-04-25 15:26:51 -0400

    refactoring

--- functions.php
+++ functions.php
@@ -22,10 +22,6 @@ function fantasyreviews_register_styles() {
         true
     );
 
-    // enqueue javascript
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
-
-
 }
 
 add_action('wp_enqueue_scripts', 'fantasyreviews_register_styles');
