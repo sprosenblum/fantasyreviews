a214b14f7ba04e0fdbf83656f267763fd05615b6
Sarah Rosenblum <rosenblum.30@osu.edu>
2022-05-02 12:48:20 -0400

    update search form

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
         <span class="screen-reader-text">
             <?php echo _x( 'Search for:', 'label' ) ?>
         </span>
-        <input type="search" class="search-field form-control me-2" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+        <input type="search" class="search-field form-control me-2" placeholder="<?php echo esc_attr_x( 'Search…', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
     </label>
     <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
 
