c3643f3e2273de4c8eca6a968adda0c875a44b51
Sarah Rosenblum <rosenblum.30@osu.edu>
2022-05-02 12:44:13 -0400

    updating footer

--- footer.php
+++ footer.php
@@ -3,18 +3,17 @@
         	<div class="col-md-3 footer-brand animated fadeInLeft">
                 <p>
                     ©<?php echo date("Y"); ?>
-                    Fantasy Reviews, All rights reserved
+                    Fantasy Reviews
                 </p>
             </div>
         	<div class="col-md-4 footer-nav animated fadeInUp">
             	<h4>Menu</h4>
             	<div class="col-md-6">
                     <ul class="pages">
-                        <li><a href="#">Travel</a></li>
-                        <li><a href="#">Nature</a></li>
-                        <li><a href="#">Explores</a></li>
-                        <li><a href="#">Science</a></li>
-                        <li><a href="#">Advice</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Children</a></li>
+                        <li><a href="#">Young-Adult</a></li>
+                        <li><a href="#">Adult</a></lis>
                     </ul>
                 </div>
 
