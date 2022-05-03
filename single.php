57a8a55957da129daccaed65a0de128b17207f4b
Sarah Rosenblum <rosenblum.30@osu.edu>
2022-05-03 12:40:55 -0400

    refactoring

--- single.php
+++ single.php
@@ -4,72 +4,71 @@
 
     <body>
 
-    <?php
-    if(have_posts()) :
-
-        while(have_posts())  : the_post(); ?>
-
-            <div class="row mb-5 mt-5">
-                <div class="col-12 col-md-2 offset-md-1 d-flex flex-column align-items-center">
-                    <img
-                        class="book-img mb-2"
-                        src="<?php echo get_the_post_thumbnail_url(); ?>"
-                        alt="Book Image"
-                    />
-                </div>
-
-
-                <div class="col-12 offset-md-1 col-md-6 text-center text-md-start">
-                    <h3 class="item-heading">
-
-                        <span class="book-item-name">
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_title(); ?>
-                            </a>
-                        </span>
-
-                        <!-- render author name onto page -->
-                        <p class="book-author mt-2 fs-5">
-                            <?php echo get_post_meta($post->ID, 'author', true); ?>
-                        </p>
-
-                    </h3>
-
-                    <p class="mt-3">
-                        <?php the_content(); ?>
-                    </p>
-
-
-                    <?php
-                    $category = get_the_category(get_post_meta($post->ID, 'category'));
-
-                    if ($category && $category[0]) { ?>
-
-                        <p class="single-category">
-                            Category:
-                            <a href="/bookreviews/category-custom-page/?category=<?php echo $category[0]->term_id; ?>
-                            ">
-                                <?php print_r( $category[0]->name ); ?>
-                            </a>
-                        </p>
-                        <?php } ?>
-                </div>
-
-                <div class="text-center mt-5 mb-3">
-                    <a href="<?php echo home_url(); ?>" class="btn btn-dark">Return Home</a>
-                </div>
-
-
         <?php
-        endwhile;
+        if(have_posts()) :
 
-    endif; ?>
+            while(have_posts())  : the_post(); ?>
+
+                <div class="row mb-5 mt-5">
+                    <div class="col-12 col-md-2 offset-md-1 d-flex flex-column align-items-center">
+                        <img
+                            class="book-img mb-2"
+                            src="<?php echo get_the_post_thumbnail_url(); ?>"
+                            alt="Book Image"
+                        />
+                    </div>
 
 
-    <?php get_footer(); ?>
+                    <div class="col-12 offset-md-1 col-md-6 text-center text-md-start">
+                        <h3 class="item-heading">
+
+                            <span class="book-item-name">
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </span>
+
+                            <!-- render author name onto page -->
+                            <p class="book-author mt-2 fs-5">
+                                <?php echo get_post_meta($post->ID, 'author', true); ?>
+                            </p>
+
+                        </h3>
+
+                        <p class="mt-3">
+                            <?php the_content(); ?>
+                        </p>
+
+
+                        <?php
+                        $category = get_the_category(get_post_meta($post->ID, 'category'));
+
+                        if ($category && $category[0]) { ?>
+
+                            <p class="single-category">
+                                Category:
+                                <a href="/bookreviews/category-custom-page/?category=<?php echo $category[0]->term_id; ?>
+                                ">
+                                    <?php print_r( $category[0]->name ); ?>
+                                </a>
+                            </p>
+                            <?php } ?>
+                    </div>
+
+                    <div class="text-center mt-5 mb-3">
+                        <a href="<?php echo home_url(); ?>" class="btn btn-dark">Return Home</a>
+                    </div>
+
+
+            <?php
+            endwhile;
+
+        endif; ?>
 
     </body>
 
-    </html>
+</html>
 
 </div>
+
+<?php get_footer(); ?>
