511ce290a0776796371ebf78560b754bad29e87f
Sarah Rosenblum <rosenblum.30@osu.edu>
2022-06-01 13:35:06 -0400

    updating to allow mobile functionality

--- header.php
+++ header.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
+<?php wp_head(); ?>
 
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="<?php bloginfo( 'charset' ); ?>">
             <title><?php wp_title( '|', true, 'right' ); ?></title>
             <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
